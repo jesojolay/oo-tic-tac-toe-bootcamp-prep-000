a33de4846cd51b8469c4dbd25990424863cdef1e
jasmine Magdi <jesojolay@yahoo.com>
2017-09-20 00:41:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,4 +3,15 @@ class TicTacToe
     def initialize(board = nil)
         @board = Array.new(9," ")
     end
+
+    WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,4,8],  # Top-left diagonals
+  [2,4,6],  # Top-right diagonals
+  [0,3,6],  # left vertical
+  [1,4,7],  # Middle vertical
+  [2,5,8]  # Right vertical
+]
 end     
