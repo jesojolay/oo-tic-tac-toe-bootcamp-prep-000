b9110b668c5c264d3a2a06cdb60d96c262042cd5
jasmine Magdi <jesojolay@yahoo.com>
2017-09-20 00:50:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,4 +49,14 @@ end
 return counter
 end
 
+def current_player(board)
+    if turn_count(board) == 0
+        return "X"
+    elsif turn_count(board).even?
+        return "X"
+    elsif turn_count(board).odd?
+        return "O"
+    end
+end
+
 end
